73fef37af08d0bd1d0d8ace0468d4835915f6f40
Noxolo <wendyngcobo98@gmail.com>
2024-05-15 21:08:34 +0200

    refactor the for loop used for both genre and author obj

--- scripts.js
+++ scripts.js
@@ -30,18 +30,24 @@ function createBookPreview(fragment, slicedObject){            //CreateBookPrevi
     document.querySelector('[data-list-items]').appendChild(fragment)
 }
 
+function createObjectEntries(objectWithEntries, fragment){
+
+    for (const [id, name] of Object.entries(objectWithEntries)) {
+        const element = document.createElement('option')
+        element.value = id
+        element.innerText = name
+        fragment.appendChild(element)
+    }
+
+}
+
 const genreHtml = document.createDocumentFragment()
 const firstGenreElement = document.createElement('option')
 firstGenreElement.value = 'any'
 firstGenreElement.innerText = 'All Genres'
 genreHtml.appendChild(firstGenreElement)
 
-for (const [id, name] of Object.entries(genres)) {
-    const element = document.createElement('option')
-    element.value = id
-    element.innerText = name
-    genreHtml.appendChild(element)
-}
+
 
 document.querySelector('[data-search-genres]').appendChild(genreHtml)
 
